0150c5fc8e7b4e8f13b1aa8080d2c24a1b334711
PiotrGrzechnik <ppgrzechnik@gmail.com>
2023-11-07 22:08:29 +0100

    Comment improting icons

--- src/app-index.ts
+++ src/app-index.ts
@@ -5,8 +5,8 @@ import './pages/app-home';
 import './components/header';
 import './styles/global.css';
 import { router } from './router';
-import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.2/cdn/components/icon/icon.js';
-import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+// import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.2/cdn/components/icon/icon.js';
+// import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 
 @customElement('app-index')
 export class AppIndex extends LitElement {
